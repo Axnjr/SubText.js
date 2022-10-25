924f54c658f7646c32924947a0a85c884002f348
Yakshit Chhipa <111436589+Axnjr@users.noreply.github.com>
2022-10-25 12:39:04 +0530

    Add files via upload

---
+++ SubElements.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SubElements(props) {
+  return (
+    <>
+        {
+            props.text.split("").map((val,id)=>{
+                return <props.element key={id} className={props.class_of_element} >{val}</props.element>
+            })
+        }
+    </>
+  )
+}
